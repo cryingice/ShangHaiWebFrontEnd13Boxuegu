ac0f78762764d58f42489135189796af38be10ce
duanlinlin <duanlinlin@itcast.cn>
2017-10-15 09:57:41 +0800

    讲师管理系统编辑/添加讲师功能实现

--- views/static/js/teacher/manage.js
+++ views/static/js/teacher/manage.js
@@ -1,4 +1,4 @@
-define(["jquery","utils","template","form"],function($,utils,template){
+define(["jquery","utils","template","form","datepicker","datepickerCN"],function($,utils,template){
 	// //实现添加教师的功能
 	// $(".teacher-add .btn-addinfo").click(function(){
 	// 	$("#teacher-info").ajaxSubmit({
@@ -17,7 +17,7 @@ define(["jquery","utils","template","form"],function($,utils,template){
 	if (id) {
 		data.url= "/api/teacher/update";
 		data.title="讲师编辑";
-		data.button="save";
+		data.bt="save";
 		$.ajax({
 			url:"/api/teacher/edit",
 			data:{
@@ -28,15 +28,42 @@ define(["jquery","utils","template","form"],function($,utils,template){
 				// $("#checkout_info_list").html(template("teacher_list_tpltwo",data.result));
 				data.teacher=msg.result;
 				$("#teacher-manage").html(template("editor-info-tpl",data));
+				$("input[name='tc_join_date']").datepicker({
+					format: "yyyy-mm-dd",
+					autoclose: true,
+            		language: "zh-CN"
+				});
 			}
 		})
 	}else{
 		data.title = "讲师添加";
-        data.buttonText = "添 加";
+        data.bt = "添 加";
         data.url = "/api/teacher/add";
         data.teacher = {};
         console.log()
         $("#teacher-manage").html(template("editor-info-tpl",data));
+        $("input[name='tc_join_date']").datepicker({
+					format: "yyyy-mm-dd",
+					autoclose: true,
+            		language: "zh-CN"
+				});
 	}
+
+	$(".teacher").on("submit","form",function(){
+		// alert(1);
+		
+		$(this).ajaxSubmit({
+		type:"post",
+		success:function(data){
+			console.log(data);
+			if (data.code==200) {
+				// location.href="/teacher/list";
+			}
+			
+		}
+	})
+		return false;
+	})
+	
 
 })
